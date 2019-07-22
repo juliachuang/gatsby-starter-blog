2e81592a6269dab6abd0ffc096bc2e70f0dd57ac
Julia Chuang <juchuang@expedia.com>
2019-07-22 11:43:22 -0700

    Put up some notes

--- src/components/newholly.js
+++ src/components/newholly.js
@@ -13,18 +13,26 @@ function NewHollyInfo() {
       <p>The homes were put by Polygon construction</p>
       <ul>
         <li>
-          Internet - Xfinity is the only real option for quick internet in the
-          area. Century Link as of 2019 only provides up to 7mbps down
+          Internet
+          <p>Xfinity is the only real option for quick internet in the
+          area. Century Link as of 2019 only provides up to 7mbps down</p>
           <ul>
             <li>Comcast Xfinity</li>
             <li>Century Link </li>
           </ul>
         </li>
         <li>
-          Trash Pickup -{" "}
+          Trash Pickup Schedule
           <a href={"https://www.seattlehousing.org/homeowners"}>
             https://www.seattlehousing.org/homeowners
           </a>
+        </li>
+        <li>Utilities
+          <ul>
+            <li>Water/Sewer is covered in the HOA payments. This is because homes are not individually metered. In fact, it's not just each building, but groups of buildings</li>
+            <li></li>
+            <li>Trash pickup is provided by Seattle Housing Authority (SHA) </li>
+          </ul>
         </li>
       </ul>
     </div>
