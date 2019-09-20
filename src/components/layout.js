452ca5d57343176e2c2b98c00a57554299185ed0
Julia Chuang <juchuang@expedia.com>
2019-09-20 14:49:20 -0700

    Update description

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Jeff & Julia' },
+              { name: 'keywords', content: 'julia, chuang, jeffrey, jeff, li' },
             ]}
           >
             <html lang="en" />
